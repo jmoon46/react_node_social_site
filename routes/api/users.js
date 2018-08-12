d09794dec10e190518f2975fc1af8b2eed89a00a
John Mooney <jrmint46@gmail.com>
2018-08-12 17:12:28 -0400

    Added login validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User model
 const User = require('../../models/User');
@@ -68,6 +69,14 @@ router.post('/register', (req, res) => {
 // @access Public
 
 router.post('/login', (req, res) => {
+
+	const { errors, isValid } = validateLoginInput(req.body);
+
+	// Check Validation
+	if (!isValid) {
+		return res.status(400).json(errors);
+	}
+
 	const email = req.body.email;
 	const password = req.body.password;
 
@@ -76,7 +85,8 @@ router.post('/login', (req, res) => {
 		.then(user => {
 			// Check for user
 			if(!user) {
-				return res.status(404).json({email: "User email not found"});
+				errors.email = "User not found"
+				return res.status(404).json(errors);
 			}
 
 			// Check Password
@@ -98,6 +108,7 @@ router.post('/login', (req, res) => {
 								})
 						});
 					} else {
+						errors.password = 'Password incorrect';
 						return res.status(400).json({password: 'Password incorrect'});
 					}
 				});

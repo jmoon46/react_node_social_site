4bba2045d4d4d2692da4873873b902b06dbbe3e8
John Mooney <jrmint46@gmail.com>
2018-10-16 18:32:35 -0400

    Made beginning of CreateProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
@@ -8,12 +8,15 @@ import { clearCurrentProfile } from './actions/profileActions';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import PrivateRoute from './components/common/PrivateRoute';
+
 import Navbar from './components/layout/Navbar.js';
 import Footer from './components/layout/Footer.js';
 import Landing from './components/layout/Landing.js';
 import Register from './components/auth/Register.js';
 import Login from './components/auth/Login.js';
 import Dashboard from './components/dashboard/Dashboard.js';
+import CreateProfile from './components/create-profile/CreateProfile.js';
 
 import './App.css';
 
@@ -49,7 +52,12 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
-              <Route exact path="/dashboard" component={ Dashboard } />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/create-profile" component={ CreateProfile } />
+              </Switch>
             </div>
             <Footer />
           </div>

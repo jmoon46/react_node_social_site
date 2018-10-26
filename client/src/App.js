f15eab5050c62841522001876b3f89893b1e7118
John Mooney <jrmint46@gmail.com>
2018-10-26 19:45:24 -0400

    Added initial profile page

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import EditProfile from './components/edit-profile/EditProfile.js';
 import AddExperience from './components/add-credentials/AddExperience.js';
 import AddEducation from './components/add-credentials/AddEducation.js';
 import Profiles from './components/profiles/Profiles.js';
+import Profile from './components/profile/Profile.js';
 
 import './App.css';
 
@@ -57,6 +58,7 @@ class App extends Component {
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
               <Route exact path="/profiles" component={ Profiles } />
+              <Route exact path="/profile/:handle" component={ Profile } />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={ Dashboard } />
               </Switch>

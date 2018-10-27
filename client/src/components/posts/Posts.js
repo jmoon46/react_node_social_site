be33ecaeaecd6521371f21e57b9f8ab7dc67dd95
John Mooney <jrmint46@gmail.com>
2018-10-27 19:37:48 -0400

    Began reducer and actions setup for get posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,15 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import PostForm from './PostForm';
+import PostFeed from './PostFeed';
+import { getPosts } from '../../actions/postActions';
 
 class Posts extends Component {
+	componentDidMount() {
+		this.props.getPosts();
+	}
+
 	render() {
+		const { posts, loading } = this.props.posts;
+		let postContent;
+
+		if(posts === null || loading) {
+			postContent = <h1>Loading...</h1>
+		} else {
+			postContent = <PostFeed posts={posts} />
+		}
+
 		return (
 			<div className="feed">
 				<div className="container">
 					<div className="row">
 						<div className="col-md-12">
 							<PostForm />
+							{postContent}
 						</div>
 					</div>
 				</div>
@@ -19,4 +35,13 @@ class Posts extends Component {
 	}
 }
 
-export default Posts;
+Posts.propTypes = {
+	getPosts: PropTypes.func.isRequired,
+	post: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+	post: state.post
+})
+
+export default connect(mapStateToProps, { getPosts })(Posts);

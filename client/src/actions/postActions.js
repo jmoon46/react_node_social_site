9b4ec3a94f2acf64ccad2cafac3940739535bb9e
John Mooney <jrmint46@gmail.com>
2018-10-27 20:20:39 -0400

    Added ability to delete posts

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from './types';
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING, DELETE_POST } from './types';
 
 // Add Post
 export const addPost = postData => dispatch => {
@@ -31,6 +31,23 @@ export const getPosts = () => dispatch => {
 		dispatch({
 			type: GET_POSTS,
 			payload: null
+		})
+	);
+}
+
+// Delete Post
+// Add Post
+export const deletePost = id => dispatch => {
+	axios.delete(`/api/posts/${id}`)
+	.then(res =>
+		dispatch({
+			type: DELETE_POST,
+			payload: id
+		})
+	).catch(err =>
+		dispatch({
+			type: GET_ERRORS,
+			payload: err.response.data
 		})
 	);
 }
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classnames from 'classnames';
 import { Link } from 'react-router-dom';
+import { deletePost } from '../../actions/postActions';
 
 class PostItem extends Component {
 	onDeleteClick(id) {
-		console.log(id);
+		this.props.deletePost(id);
 	}
 
 	render() {
@@ -48,6 +49,7 @@ class PostItem extends Component {
 }
 
 PostItem.propTypes = {
+	deletePost: PropTypes.func.isRequired,
 	post: PropTypes.object.isRequired,
 	auth: PropTypes.object.isRequired
 }
@@ -56,4 +58,4 @@ const mapStateToProps = state => ({
 	auth: state.auth
 });
 
-export default connect(mapStateToProps)(PostItem);
+export default connect(mapStateToProps, { deletePost })(PostItem);
